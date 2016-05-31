22d1709d59728581aa4c12169ec455a15e946f7e
ana.ferreira <ana.caroline.ferreir@gmail.com>
2016-05-31 14:20:23 -0300

    i18n vraptor

--- src/main/java/cih/projects/controller/IndexController.java
+++ src/main/java/cih/projects/controller/IndexController.java
@@ -1,5 +1,9 @@
 package cih.projects.controller;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import javax.inject.Inject;
 
 import br.com.caelum.vraptor.Controller;
@@ -12,6 +16,11 @@ public class IndexController {
 	private Result result;
 	@Get("/")
 	public void index() {
+		Date date = new Date();
+		Calendar calendar = new GregorianCalendar();
+		calendar.setTime(date);
+		result.include("date", calendar);
+		result.include("meuNumero", 12);
 	}
 	@Get("/home")
 	public void home() {
