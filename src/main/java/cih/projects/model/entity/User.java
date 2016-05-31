c2ffc3af35fc9c070b2ccfb4823a5ddb30ce5063
ana.ferreira <ana.caroline.ferreir@gmail.com>
2016-05-31 10:09:29 -0300

    hibernate creating table

--- src/main/java/cih/projects/model/entity/User.java
+++ src/main/java/cih/projects/model/entity/User.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -15,6 +16,7 @@ import org.hibernate.tool.hbm2ddl.SchemaExport;
 import org.hibernate.validator.constraints.Email;
 
 @Entity
+@Table(name="user_table")
 public class User implements Serializable{
 	/**
 	 * 
@@ -36,6 +38,7 @@ public class User implements Serializable{
 	@NotNull
 	@Email
 	private String email;
+	@Transient
 	@NotNull
 	@Email
 	private String confirmEmail;
